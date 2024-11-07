d4a5cd36a0e803fc5116ccb849682d50b0cb5a94
sonali sahu <sonalisahu0501@gmail.com>
2024-11-07 19:30:05 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", router);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`App is listening on PORT : ${port}`);
+});
